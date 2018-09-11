a9b594d97fc0ff241c7cc77b0b18043da47530ac
khalv <kalle_halvarsson@live.se>
2018-09-11 16:47:49 +0200

    Fix faulty reparenting of geosets

--- export_mdl/export_mdl.py
+++ export_mdl/export_mdl.py
@@ -470,6 +470,9 @@ def get_parent(obj):
     if obj.parent_type == 'BONE': #TODO: Check if animated - otherwise, make it a helper
         return obj.parent_bone if obj.parent_bone != "" else None
         
+    if parent.type == 'EMPTY' and parent.name.startswith("Bone_"):
+        return parent.name
+        
     anim_loc = get_curves(parent, 'location', (1, 2, 3))
     anim_rot = get_curves(parent, 'rotation_quaternion', (1, 2, 3, 4))
     anim_scale = get_curves(parent, 'scale', (1, 2, 3))
@@ -479,12 +482,6 @@ def get_parent(obj):
         root_parent = get_parent(parent)
         if root_parent is not None:
             return root_parent
-    
-    if parent.type in {'MESH', 'EMPTY', 'ARMATURE'}:
-        if parent.name.startswith("Bone_"):
-            return parent.name
-        else:
-            return "Bone_"+parent.name
             
     return get_parent(parent)
   
